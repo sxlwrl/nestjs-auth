6b56125e83a53e3202853c929c73fff1f8eca766
sxlwrl <vadymkazak2019@gmail.com>
2024-05-04 21:40:01 +0300

    [add] users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { UserExistsGuard } from '../guards/user-exists.guard';
+import { AccessGuard } from '../guards/access.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(UserExistsGuard)
+  @UseGuards(AccessGuard)
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() data: UpdateUserDto) {
+    return this.usersService.updateUser(Number(id), data);
+  }
+
+  @UseGuards(UserExistsGuard)
+  @UseGuards(AccessGuard)
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(Number(id));
+  }
+}
