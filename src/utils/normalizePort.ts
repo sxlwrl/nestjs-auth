3ba6db6285dfeca91e58e299789ea2179d95c0cd
sxlwrl <vadymkazak2019@gmail.com>
2024-05-04 21:34:21 +0300

    [add] normalize port util function

---
+++ src/utils/normalizePort.ts
@@ -0,0 +1,19 @@
+function normalizePort(val: unknown) {
+  if (typeof val !== 'string') {
+    throw new Error('Incorrect port type');
+  }
+
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+export default normalizePort;
